a8915483ac7db082a4a030ab4a1c0b5bea469ff7
unicornx <unicorn_wang@outlook.com>
2020-11-20 16:28:08 +0800

    fixed issue for making rootfs image

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -5,6 +5,7 @@ PATH_OUT=$PATH_PRJ/out
 
 rm -f $PATH_OUT/rootfs.img
 $PATH_QEMU/qemu-img create $PATH_OUT/rootfs.img 1g
+chmod 666 $PATH_OUT/rootfs.img
 mkfs.ext4 $PATH_OUT/rootfs.img
 mkdir -p $PATH_OUT/rootfs
 mount -o loop $PATH_OUT/rootfs.img  $PATH_OUT/rootfs
@@ -20,7 +21,7 @@ cat > $PATH_OUT/rootfs/etc/init.d/rcS <<EOF
 #!/bin/sh
 mount -t proc none /proc
 mount -t sysfs none /sys
-/sbin/mdev -s
+/usr/bin/mdev -s
 EOF
 chmod +x $PATH_OUT/rootfs/etc/init.d/rcS
 umount $PATH_OUT/rootfs
